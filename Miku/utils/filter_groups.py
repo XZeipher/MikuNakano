577312e7dab123fa2086e1b2c3ad30e833783870
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-07 12:08:26 +0530

    Add files via upload

---
+++ Miku/utils/filter_groups.py
@@ -0,0 +1,12 @@
+
+afk_watcher = 1
+flood_watcher = 11
+auto_reverse_watcher = 8
+chat_watcher = 8
+welcome_watcher = 7
+game_watcher1 = 3
+game_watcher2 = 4
+forsesub_watcher = 6
+filter_watcher = 7
+blacklist_sticker_watcher = 3
+blacklist_watcher = 6
