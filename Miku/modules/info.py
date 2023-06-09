f35c11034c4282a677bc5381a86ed2f2c3caa20d
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-09 18:02:44 +0530

    Add files via upload

--- Miku/modules/info.py
+++ Miku/modules/info.py
@@ -1,34 +1,36 @@
 import random
 import os
 from Miku import app
-from pyrogram import filters ,enums
+from pyrogram import filters, enums
 from unidecode import unidecode
-from Miku.modules.pyro.status import user_admin,user_can_change_info            
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from Miku.modules.pyro.status import user_admin, user_can_change_info
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from Miku.utils.info_data import miku
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , CallbackQuery 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
-async def check_temp(chat_id,pfp,name,chat_title,user_id,username):
+
+async def check_temp(chat_id, pfp, name, chat_title, user_id, username):
     chat_title = unidecode(chat_title).upper()
-    welpic = await miku(pfp=pfp,chat=chat_title,id=user_id)
+    welpic = await miku(pfp=pfp, chat=chat_title, id=user_id)
     return welpic
 
+
 @app.on_message(filters.command("info") & filters.group)
-async def inform(_,msg):
-	chat_id = msg.chat.id
-	chat_title = msg.chat.title
-	for m in msg.from_user:
-        name = m.first_name 
+async def inform(_, msg):
+    chat_id = msg.chat.id
+    chat_title = msg.chat.title
+    for m in msg.from_user:
+        name = m.first_name
         username = m.username
-        mention = m.mention 
-        user_id = m.id       
+        mention = m.mention
+        user_id = m.id
         try:
             user_photo = m.photo.big_file_id
-            pic = await _.download_media(m.photo.big_file_id,file_name =f"pp{user_id}.png")
+            pic = await _.download_media(m.photo.big_file_id, file_name=f"pp{user_id}.png")
         except AttributeError:
             pic = "./Miku/resources/profilepic.png"
-        welpic = await check_temp(chat_id,pic,name,chat_title,user_id,username)
-        await _.send_photo(chat_id,welpic)
+        welpic = await check_temp(chat_id, pic, name, chat_title, user_id, username)
+        await _.send_photo(chat_id, welpic)
         os.remove(welpic)
         if user_photo:
             os.remove(pic)
