34d9e4dcf6daab47a3dd499b92d75df08c289e26
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-20 20:59:52 +0530

    Add files via upload

---
+++ Miku/modules/ytdl.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+from pyrogram import filters
+import os
+from Miku import app
+
+@app.on_message(filters.command("ytdl") & filters.private)
+async def download_youtube_video(client, message):
+    video_url = message.text.split("/ytdl")[1]
+    x = await message.reply_text("**Processing...**")
+    try:
+        yt = YouTube(video_url)
+        video = yt.streams.get_highest_resolution()
+        start_time = 10
+        end_time = start_time + 5
+        await x.edit(f"**Downloading:** {yt.title}")
+        video.download(filename="temp")
+        original_filename = video.default_filename
+        new_filename = original_filename.replace("temp", "")
+        video_file = "alpha.mp4"
+        temp_file = f"temp/{original_filename}"
+        os.rename(temp_file, video_file)
+        await x.delete()
+        await message.reply_video(video_file, caption=yt.title)
+    except Exception as e:
+        print("Error:", str(e))
