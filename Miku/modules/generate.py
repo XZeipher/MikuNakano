be0531502d702466fc89c256cbd3b0921beec082
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-20 11:48:36 +0530

    Add files via upload

--- Miku/modules/generate.py
+++ Miku/modules/generate.py
@@ -11,7 +11,8 @@ async def gen(client , msg):
     openai.api_key = key
     response = openai.Image.create(prompt=text, n=1, size="1024x1024")
     image_url = response['data'][0]['url']
-    await x.edit(photo=image_url , caption=f"**Prompt:-**\n{text}")
+    await msg.reply_photo(photo=image_url , caption=f"**Prompt:-**\n{text}")
+    await text.delete()
 
 
 
