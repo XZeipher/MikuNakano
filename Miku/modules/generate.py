291667edeff6aefdc20eea7cee900bd225d1a957
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-21 14:05:50 +0530

    Add files via upload

--- Miku/modules/generate.py
+++ Miku/modules/generate.py
@@ -128,39 +128,3 @@ async def handle_callback(client, callback_query):
             output_url = None
         await x.delete()
         await callback_query.message.reply_photo(photo=output_url, caption=f"**Prompt:-**\n{text}")
-
-/eval import requests  
-import json  
-  
-url =  "https://stablediffusionapi.com/api/v3/dreambooth"  
-  
-payload = json.dumps({  
-"key":  "zgisLerC5jxfHwJIedKECUTTBXTGFY9uXTD6dagWI2jYGHopfEfvMZx0GFFx",  
-"model_id":  "dark-sushi-mix",  
-"prompt":  "actual 8K portrait photo of gareth person, portrait, happy colors, bright eyes, clear eyes, warm smile, smooth soft skin, big dreamy eyes, beautiful intricate colored hair, symmetrical, anime wide eyes, soft lighting, detailed face, by makoto shinkai, stanley artgerm lau, wlop, rossdraws, concept art, digital painting, looking into camera",  
-"negative_prompt":  "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",  
-"width":  "512",  
-"height":  "512",  
-"samples":  "1",  
-"num_inference_steps":  "30",  
-"safety_checker":  "yes",  
-"enhance_prompt":  "yes",  
-"seed":  None,  
-"guidance_scale":  7.5,  
-"multi_lingual":  "yes",  
-"panorama":  "yes",  
-"self_attention":  "yes",  
-"upscale":  "yes",  
-"embeddings":  "embeddings_model_id",  
-"lora":  "lora_model_id",  
-"webhook":  None,  
-"track_id":  None  
-})  
-  
-headers =  {  
-'Content-Type':  'application/json'  
-}  
-  
-response = requests.request("POST", url, headers=headers, data=payload)  
-  
-print(response.text)
