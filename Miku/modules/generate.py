8f730da4ef38a53f7c1c33db67b44a211605b9be
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-21 21:14:46 +0530

    Add files via upload

--- Miku/modules/generate.py
+++ Miku/modules/generate.py
@@ -5,126 +5,48 @@ import openai
 import random
 import requests
 import json
+from Miku.modules.utils.misc import get_text, post_
 
 key = "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"
 text = ""
-API_KEY = [
-    "mfDX5RWRfp3drRlJrhKnUESfAEcvBsanwvyTNC4tfg0ELbXrK10OZ3YI7cPG",
-    "UphNOVzEDLSZvZR9VBMGsjLfPnQ3yh2qh0dGgLq6jbKYk1EVHUp3r4aX1cIo",
-    "bGp6ZyGJAkVBisvIPRMUUM5AtCLTUen0W2Br1Thp5Uzl1d38sdMsd436Ns6Z",
-    "E2KcVIEb2z5OZcJzTeZLaGIAByB0V0g0asROjC681tYsHZ6LkQFU6n1j5TO6",
-    "nwlu0MD33EHSxsLWZxVN8CDVZd9zOzIdj97BBRrSNgndcLyYg3nSZixiDw4t",
-    "LacoWFS1YgGNdWxmZsvoko7cqXc8T8aYCk6PN6oNQVVnxkDxcUYwHTs1XBAS",
-    "8gN6LPbVyPprzc6iqEm6rGscpcgkpVC2MSHP1NqW3wc7dePulBx3QhSYQ48d",
-    "PpwVYZSGIPS6GSyxWpGy0UJCBsSgEIN7WHevNzEL9yK0oG4V9GqOKWe1SFqP",
-    "KGSD1PEtmmlxq386JC02oi05tadZIFqioCPydI58O9Op5TMrWufSnjpYh3Sn",
-    "6GemRIXq87NuC3XW8owVqkzVPn10jsJIFwpLt6UtLnaOupLNjs5tQDkP0WRA",
-    "qlBt8dp7qJjo6PBZaWfkUpdolVRTSqP7hF6JF4IFN3ahopdCwTY9H7u5oXwU",
-    "PEGpyq2jYDHzTztjKW6cZG9IIVCe7vroYwWepnAtWgtbPguzaMX1xXJMaUL5",
-    "zgisLerC5jxfHwJIedKECUTTBXTGFY9uXTD6dagWI2jYGHopfEfvMZx0GFFx",
-    "M7M5ePQKzImLRGB3KdtyOJtAknsnOCm2mFCB75WMmUr7Djwb93B0FzfGinLa",
-    "1hgz5P9hcuVO9CE46vhkMjFSMkEYdFisq0JHn1of5tzjNsaY8kobM2UHSgO5"
-]
 
+MODELSS = {"Dall-E":0,"Dark Sushi":"dark-sushi-25d","Guofeng3":"guofeng3","Meina Mix":"meinamix"}
+MODELS_LIST = List(MODELSS)
 
 @app.on_message(filters.command("gen"))
 async def generate(client, message):
     global text
-    text = message.text.split("/gen")[1]
-    button1 = InlineKeyboardButton("DALL-E", callback_data="button1")
-    button2 = InlineKeyboardButton("Dark Sushi", callback_data="button2") 
-    button3 = InlineKeyboardButton("Guofeng3", callback_data="button3") 
-    button4 = InlineKeyboardButton("Meina Mix", callback_data="button4") 
+    text = await get_text(message)
+    button1 = InlineKeyboardButton("DALL-E", callback_data=f"gen.0.{message.from_user.id}")
+    button2 = InlineKeyboardButton("Dark Sushi", callback_data=f"gen.0.{message.from_user.id}") 
+    button3 = InlineKeyboardButton("Guofeng3", callback_data=f"gen.0.{message.from_user.id}") 
+    button4 = InlineKeyboardButton("Meina Mix", callback_data=f"gen.0.{message.from_user.id}") 
     keyboard = InlineKeyboardMarkup([[button1, button2 , button3 , button4]])
     await message.reply_text("**Select a model.**", reply_markup=keyboard)
 
 
-@app.on_callback_query()
-async def handle_callback(client, callback_query):
-    callback_data = callback_query.data
-    if callback_data == "button1":
-        x = await callback_query.message.edit_text("**Creating Artificial Image...**")
+@app.on_callback_query(filters.regex(pattern=r"^gen.(.*)"))
+async def generatebtns(client, query):
+    global text
+    data = query.data.split('.')
+    auth_user = int(data[-1])
+    model = int(data[1])
+    if auth_user != query.from_user.id:
+        await query.answer("Could you fuck off over there?")
+        return
+    if model == 0:
+        x = await query.message.edit_text("**Creating Artificial Image...**")
         openai.api_key = key
         response = openai.Image.create(prompt=text, n=1, size="1024x1024")
         image_url = response['data'][0]['url']
         await x.delete()
-        await callback_query.message.reply_photo(photo=image_url, caption=f"**Prompt:-**\n{text}")
-    elif callback_data == "button2":
-        x = await callback_query.message.edit_text("**Creating Artificial Image...**")
-        url = "https://stablediffusionapi.com/api/v4/dreambooth"
-        payload = json.dumps({
-            "key": random.choice(API_KEY),
-            "model_id": "dark-sushi-25d",
-            "prompt": text,
-            "negative_prompt":  "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",  
-            "width":  "512",  
-            "height":  "512",  
-            "samples":  "1",  
-            "num_inference_steps":  "30",  
-            "safety_checker":  "yes",  
-            "enhance_prompt":  "yes",  
-            "seed":  None,  
-            "guidance_scale":  7.5,  
-            "multi_lingual":  "yes",  
-            "panorama":  "yes",  
-            "self_attention":  "yes",  
-            "upscale":  "yes",  
-            "embeddings":  "embeddings_model_id",  
-            "lora":  "lora_model_id",  
-            "webhook":  None,  
-            "track_id":  None     
-        })
-        headers = {
-          'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers, data=payload).json()
-        if "output" in response and response["output"]:
-            output_url = response["output"][0]
-        elif "future_links" in response and response["future_links"]:
-            output_url = response["future_links"][0]
-        else:
-            output_url = None
+        await query.message.reply_photo(photo=image_url, caption=f"**Prompt:-**\n{text}")
+    elif model == 1 or model == 2 or model == 3:
+        MODEL_ID = MODELSS[MODELS_LIST[model]]
+        output_url = await post_(text,MODEL_ID)
+        if output_url is None:
+            await query.edit_message_text("Something went wrong!")
+            return
         await x.delete()
-        await callback_query.message.reply_photo(photo=output_url, caption=f"**Prompt:-**\n{text}")
-    elif callback_data == "button3":
-        x = await callback_query.message.edit_text("**Creating Artificial Image...**")
-        url = "https://stablediffusionapi.com/api/v4/dreambooth"
-        payload = json.dumps({
-            "key": random.choice(API_KEY),
-            "model_id": "guofeng3",
-            "prompt": text,
-            "negtive-prompt": "drawing, extra legs, extra body, extra hand, cartoon, weird face"          
-        })
-        headers = {
-          'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers, data=payload).json()
-        if "output" in response and response["output"]:
-            output_url = response["output"][0]
-        elif "future_links" in response and response["future_links"]:
-            output_url = response["future_links"][0]
-        else:
-            output_url = None
-        await x.delete()
-        await callback_query.message.reply_photo(photo=output_url, caption=f"**Prompt:-**\n{text}")
-    elif callback_data == "button4":
-        x = await callback_query.message.edit_text("**Creating Artificial Image...**")
-        url = "https://stablediffusionapi.com/api/v4/dreambooth"
-        payload = json.dumps({
-            "key": random.choice(API_KEY),
-            "model_id": "meinamix",
-            "prompt": text,
-            "negtive-prompt": "drawing, extra legs, extra body, extra hand, cartoon, weird face"          
-        })
-        headers = {
-          'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers, data=payload).json()
-        if "output" in response and response["output"]:
-            output_url = response["output"][0]
-        elif "future_links" in response and response["future_links"]:
-            output_url = response["future_links"][0]
-        else:
-            output_url = None
-        await x.delete()
-        await callback_query.message.reply_photo(photo=output_url, caption=f"**Prompt:-**\n{text}")
+        await query.message.reply_photo(photo=output_url, caption=f"**Prompt:-**\n{text}")
+
