2afa1029f8c5b1e70634f1f5de4a61d9d4aabe95
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-06-24 09:58:49 +0530

    Add files via upload
    Added System Statistics

---
+++ Miku/modules/system_stats.py
@@ -0,0 +1,64 @@
+import asyncio
+import os
+import subprocess
+import time
+import psutil
+from pyrogram import filters
+from Miku import StartTime
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
+
+# Convert seconds to mm:ss
+async def convert_seconds_to_minutes(seconds: int):
+    seconds = int(seconds)
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    users = sql.num_users()
+    chats = sql.num_chats()
+    stats = f"""
+♠ Miku's Current System Stats ♠
+
+────────────────────────
+• UPTIME: {get_readable_time((bot_uptime))}
+• BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+• CPU: {cpu}%
+• RAM: {mem}%
+• DISK: {disk}%
+• CHATS: {chats}
+• USERS: {users}
+────────────────────────
+"""
+    return stats
