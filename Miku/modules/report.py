108517a9e4c8a8516ede33e6f41303c7979a1430
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-03 21:23:39 +0530

    Add files via upload

--- Miku/modules/report.py
+++ Miku/modules/report.py
@@ -5,28 +5,30 @@ from Miku import app
 
 LOG_ID = int("-1001813613591")
 
-@app.on_message(filters.command(["report", "bug" , "feedback"]))
+@app.on_message(filters.command(["report", "bug", "feedback"]))
 async def requests(client: app, message: Message):
-	if message.chat.type == ChatType.PRIVATE:
-		return await message.reply_text("**Report Can Be Done In Groups Only.**")
+    if message.chat.type == ChatType.PRIVATE:
+        return await message.reply_text("**Report Can Be Done In Groups Only.**")
+
     text_link = message.link
-    text = message.text.split(None)[1]
+    text = message.text.split(maxsplit=1)[1]
     EVENT = InlineKeyboardMarkup(
-  [
-    [
-      InlineKeyboardButton(
-        text="Event", url=text_link
-      )
-    ]
-  ]
+        [
+            [
+                InlineKeyboardButton(
+                    text="Event", url=text_link
+                )
+            ]
+        ]
     )
 
     USER_TEXT = f'''**<a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a> your report has been successfully sent to our developers
-ThankYou :)**'''
+Thank you :)**'''
+
     DEV_TEXT = f'''**#Report
 
-User :- <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
-Text :- {text}**
-'''
-    await client.send_message(LOG_ID, DEV_TEXT,reply_markup=EVENT)
+User: <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
+Text: {text}**'''
+
+    await client.send_message(LOG_ID, DEV_TEXT, reply_markup=EVENT)
     await message.reply_text(USER_TEXT)
