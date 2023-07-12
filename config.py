d422f706c4883266a51ac142202c352797dbb9a4
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-12 08:48:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv('API_ID','9176863'))
-API_HASH = getenv('API_HASH','afff208ad0de11acfc946ca6dcd74aec')
-BOT_TOKEN = getenv('BOT_TOKEN','6106438500:AAG-ip5EpZRhbuE1zsY8S8L34DEHDKxzzqY')
+API_ID = int(getenv('API_ID','18770647'))
+API_HASH = getenv('API_HASH','ed11b8af8b51418dbac60b456d1429a7')
+BOT_TOKEN = getenv('BOT_TOKEN','5429814096:AAETs76j16YmrF50d-HLEbjJRVG2HSuH4Tg')
 MONGO_DB_URL = getenv('MONGO_DB_URL','mongodb+srv://PRIME:Ricks_2005@cluster0.koprs84.mongodb.net/?retryWrites=true&w=majority')
 SUPPORT_CHAT = getenv('SUPPORT_CHAT','MikuNakanoXSupport')
 UPDATES_CHANNEL = getenv('UPDATES_CHANNEL','MikuNakanoXUpdates')
-OWNER_ID = int(getenv('OWNER_ID','5764124248'))
-LOG_CHANNEL_ID = int(getenv('LOG_CHANNEL_ID','-1001813613591'))
+OWNER_ID = int(getenv('OWNER_ID','6393014348'))
+LOG_CHANNEL_ID = int(getenv('LOG_CHANNEL_ID','-1001805033582'))
 DEV_USERS = list(map(int, getenv("DEV_USERS", "").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1811267624").split()))
 ARQ_API_KEY = getenv('ARQ_API_KEY','DARMXR-EKRMBT-BHPDOP-UASHHF-ARQ')
 DONATION_LINK = getenv('DONATION_LINK','https://t.me/ImmortalsXKing')
  
