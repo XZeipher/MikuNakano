cbe192b6bc3caaabf24e6a28924ae3dd224bcba2
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-12 18:25:32 +0530

    Add files via upload

--- Miku/modules/whispers.py
+++ Miku/modules/whispers.py
@@ -5,21 +5,21 @@ from pyrogram.types import *
 import shortuuid
 
 
-@app.on_inline_query()
+@app.on_inline_query(filters.text)
 async def mainwhisper(_, query):
     if not query.query:
-        await query.answer([], switch_pm_text="Give me a username or ID!", switch_pm_parameter="ghelp_whisper")
-        return
+        return await query.answer([], switch_pm_text='Give me a username or ID!', switch_pm_parameter='ghelp_whisper')
 
-    user = query.query.split(' ')[0]
+    text = query.query.split(' ')
+    user = text[0]
     first = True
 
     if not user.startswith('@') and not user.isdigit():
-        user = query.query.split(' ')[-1]
+        user = text[-1]
         first = False
 
     if not user.startswith('@') and not user.isdigit():
-        await query.answer([], switch_pm_text="Give me a username or ID!", switch_pm_parameter="ghelp_whisper")
+        await query.answer([], switch_pm_text='Give me a username or ID!', switch_pm_parameter='ghelp_whisper')
         return
 
     if user.isdigit():
@@ -29,71 +29,62 @@ async def mainwhisper(_, query):
         except:
             user = user
 
-    message = ' '.join(query.query.split(' ')[1:]) if first else ' '.join(query.query.split(' ')[:1])
+    message = ' '.join(text[1:]) if first else ' '.join(text[:1])
 
     if len(message) > 200:
-        await query.answer([], switch_pm_text="Only text up to 200 characters is allowed!", switch_pm_parameter="ghelp_whisper")
+        await query.answer([], switch_pm_text='Only text up to 200 characters is allowed!', switch_pm_parameter='ghelp_whisper')
         return
 
-    results = []
+    whisperData = {'user': query.from_user.id, 'withuser': user, 'usertype': 'username', 'type': 'inline', 'message': message}
+    whisperId = shortuuid.uuid()
 
-    # Add loading state result
-    results.append(
+    # Add the whisper to the database
+    await Whispers.add_whisper(whisperId, whisperData)
+
+    answers = [
         InlineQueryResultArticle(
-            title="Generating Whisper message...",
-            input_message_content=InputTextMessageContent("Generating Whisper message...")
+            title=f' Send a whisper message to {user}!',
+            description='Only they can see it!',
+            input_message_content=InputTextMessageContent('A Whisper Message For {user}.\n Only they can see it.'),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(' Show Whisper', callback_data=f'whisper_{whisperId}')]])
         )
-    )
+    ]
 
-    # Send the initial loading state result
-    await app.answer_inline_query(query.id, results)
-
-    # Generate the actual whisper result
-    whisper_result = InlineQueryResultArticle(
-        title=f'🤫 Send a whisper message to {user}!',
-        description='Only they can see it!',
-        input_message_content=InputTextMessageContent('Generating Whisper message...'),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🤫 Show Whisper', callback_data=f'whisper_{shortuuid.uuid()}')]])
-    )
-
-    # Replace the loading state result with the actual whisper result
-    results[0] = whisper_result
-
-    # Answer the inline query with the results
-    await app.answer_inline_query(query.id, results)
+    await query.answer(answers)
 
 
-@app.on_callback_query()
-async def showwhisper(_, callback_query):
-    whisper_id = callback_query.data.split('_')[-1]
-    whisper_data = await Whispers.get_whisper(whisper_id)  # Await the call to get_whisper
-    if not whisper_data:
+@app.on_callback_query(filters.regex('^whisper_'))
+async def showWhisper(_, callback_query):
+    whisperId = callback_query.data.split('_')[-1]
+    whisper = await Whispers.get_whisper(whisperId)
+
+    if not whisper:
         await callback_query.answer("This whisper is not valid anymore!")
         return
 
-    user_type = whisper_data['usertype']
+    userType = whisper['usertype']
 
-    if callback_query.from_user.id == whisper_data['user']:
-        await callback_query.answer(whisper_data['message'], show_alert=True)
-    elif user_type == 'username' and callback_query.from_user.username and callback_query.from_user.username.lower() == whisper_data['withuser'].replace('@', '').lower():
-        await callback_query.answer(whisper_data['message'], show_alert=True)
-        await Whispers.del_whisper(whisper_id)  # Await the call to del_whisper
-        await app.send_message(callback_query.from_user.id, f"**{whisper_data['withuser']} read the Whisper.**")
-    elif user_type == 'id' and callback_query.from_user.id == int(whisper_data['withuser']):
-        user = await app.get_users(int(whisper_data['withuser']))
+    if callback_query.from_user.id == whisper['user']:
+        await callback_query.answer(whisper['message'], show_alert=True)
+    elif userType == 'username' and callback_query.from_user.username and callback_query.from_user.username.lower() == whisper['withuser'].replace('@', '').lower():
+        await callback_query.answer(whisper['message'], show_alert=True)
+        await Whispers.del_whisper(whisperId)
+        await app.send_message(callback_query.from_user.id, f"{whisper['withuser']} read the Whisper.")
+    elif userType == 'id' and callback_query.from_user.id == int(whisper['withuser']):
+        user = await app.get_users(int(whisper['withuser']))
         username = user.username or user.first_name
-        await callback_query.answer(whisper_data['message'], show_alert=True)
-        await Whispers.del_whisper(whisper_id)  # Await the call to del_whisper
-        await app.send_message(callback_query.from_user.id, f"**{username} read the whisper.**")
+        await callback_query.answer(whisper['message'], show_alert=True)
+        await Whispers.del_whisper(whisperId)
+        await app.send_message(callback_query.from_user.id, f"{username} read the whisper.")
     else:
-        await callback_query.answer("No one sent you a whisper.", show_alert=True)
+        await callback_query.answer("Not your Whisper!", show_alert=True)
 
 
 __help__ = """
 **Whisper Inline Function For Secret Chats.**
 
 **Commands**
-♠️ `@MikuNakanoXSupport your message @username OR UserID`
-♠️ `@MikuNakanoXSupport @username OR UserID your message`
+ `@MikuNakanoXSupport your message @username OR UserID`
+ `@MikuNakanoXSupport @username OR UserID your message`
 """
 __mod_name__ = "Whispers"
