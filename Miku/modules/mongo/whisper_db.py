2b46cb2dddfb376b8193963689ab7b559fc21577
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-12 17:14:15 +0530

    Add files via upload

--- Miku/modules/mongo/whisper_db.py
+++ Miku/modules/mongo/whisper_db.py
@@ -6,23 +6,22 @@ INSERTION_LOCK = threading.RLock()
 
 class Whispers:
     @staticmethod
-    def add_whisper(WhisperId, WhisperData):
+    async def add_whisper(WhisperId, WhisperData):
         with INSERTION_LOCK:
             whisper = {
                 'WhisperId': WhisperId,
                 'whisperData': WhisperData
             }
-            collection.insert_one(whisper)
+            await collection.insert_one(whisper)
 
     @staticmethod
-    def del_whisper(WhisperId):
+    async def del_whisper(WhisperId):
         with INSERTION_LOCK:
-            collection.delete_one({'WhisperId': WhisperId})
+            await collection.delete_one({'WhisperId': WhisperId})
 
     @staticmethod
-    def get_whisper(WhisperId):
-        whisper = collection.find_one({'WhisperId': WhisperId})
+    async def get_whisper(WhisperId):
+        whisper = await collection.find_one({'WhisperId': WhisperId})
         if whisper:
             return whisper['whisperData']
         return None
-
