c1f692dbf3975089903c6e858494fcf86f8cd80c
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-24 07:50:59 +0530

    Update info.py

--- Miku/modules/info.py
+++ Miku/modules/info.py
@@ -11,7 +11,89 @@ from Miku.modules.mongo.gbans_db import is_gbanned_user, get_gban_reason
 
 btn = InlineKeyboardMarkup([[InlineKeyboardButton("➕ Add Me To Your Group ➕", url="https://t.me/MikuNakanoXBot?startgroup=true")]])
 
-@app.on_message(filters.command("info"))
+@app.on_message(filters.command("info") & filters.private)
+async def _info(_, message):
+    user_id = await extract_user_id(message)
+    if not user_id:
+        user_id = message.from_user.id
+
+    msg = await message.reply("**Miku Initialisation.....**")
+    try:
+        user = await _.get_users(user_id)
+    except Exception as e:
+        if user_id:
+            return await msg.edit("**Provide Username Or Tag The User!**")
+        mx = message.text.split(maxsplit=1)[1]
+        if not mx.startswith("@"):
+            return await msg.edit("**Provide Username Or Tag The User!**")
+        if mx.startswith("@"):
+            user = await ubot.get_users(mx)
+            user_id = user.id
+        if message.reply_to_message:
+            username = message.reply_to_message.username
+            user = await ubot.get_users(username)
+            user_id = user.id
+
+    text = "━━━━━━ [INFORMATION] ━━━━━━━\n"
+    text += f"**• ID :** `{user_id}`\n"
+    text += f"**• First Name :** `{user.first_name}`\n"
+    if user.last_name:
+        text += f"**• Last Name :** `{user.last_name}`\n"
+    if user.username:
+        text += f"**• Username :** @{user.username}\n"
+    text += f"**• Profile :** {user.mention}\n"
+    text += f"**• Premium :** {user.is_premium}\n"
+    text += f"**• Gbanned :** {str(await is_gbanned_user(user_id))}\n"
+
+    if message.chat.type != enums.ChatType.PRIVATE:
+        ptext = "**• Status :** `{}`\n"
+
+        if await is_user_afk(user_id):
+            text += ptext.format("Afk")
+        else:
+            try:
+                member = await _.get_chat_member(message.chat.id, user_id)
+                if member.status in [enums.ChatMemberStatus.LEFT, enums.ChatMemberStatus.BANNED]:
+                    text += ptext.format("Not Here")
+                if member.status == enums.ChatMemberStatus.MEMBER:
+                    text += ptext.format("Member")
+                if member.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER]:
+                    text += ptext.format("Administrator")
+            except BadRequest:
+                text += ptext.format("Not Here")
+
+    try:
+        mm = await _.get_chat_member(message.chat.id, user_id)
+        if mm.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER] and mm.custom_title:
+            text += f"**• Title :** `{mm.custom_title}`\n"
+    except:
+        pass
+
+    reason = await get_gban_reason(user_id)
+    if reason:
+        text += f"**• Gban Reason :** `{reason}`\n"
+
+    if user.status:
+        text += f"**• Last Seen :** `{user.status}`\n"
+
+    if user_id == OWNER_ID:
+        text += "\n**Creator Alpha Coder !!!**"
+    elif user_id in SUPREME_USERS:
+        text += "\n**This Is Bot Admin**"
+    elif user_id in CHAD:
+        text += "\n**This Is Sun User**"
+    elif user_id == 2027056490 or user_id == 6396522589:
+        text += "\n**Alpha's Daughter!!!**"
+
+    if user.photo:
+        pic = await _.download_media(user.photo.big_file_id)
+        await _.send_photo(message.chat.id, photo=pic, caption=text, reply_markup=btn)
+        os.remove(pic)
+    else:
+        await _.send_message(message.chat.id, text, reply_markup=btn)
+
+    await msg.delete()
+@app.on_message(filters.command("info") & filters.group,group=69)
 async def _info(_, message):
     user_id = await extract_user_id(message)
     if not user_id:
