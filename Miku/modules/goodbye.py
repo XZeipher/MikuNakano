2ac1495a706c56cfc401ef6c4fd07be5cf12d635
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-27 11:19:32 +0530

    Update goodbye.py

--- Miku/modules/goodbye.py
+++ Miku/modules/goodbye.py
@@ -1,5 +1,3 @@
-# Credits :- @TheStark
-
 from Miku import app
 from pyrogram import filters
 import uuid
@@ -7,12 +5,10 @@ import math
 import os
 from PIL import Image, ImageDraw, ImageFont
 from unidecode import unidecode
-
-
-
+import asyncio
 
 @app.on_message(filters.left_chat_member)
-async def _left_mem(client,message):
+async def _left_mem(client, message):
     first_name = unidecode(message.left_chat_member.first_name)
     user_id = message.left_chat_member.id
  
@@ -28,10 +24,8 @@ async def _left_mem(client,message):
     except EOFError:
         pass
 
-    stark[0] = stark[0]
-
-    text = [f"Goodbye {first_name}!"]
-
+    
+    text = ["World opens itself"]
     s1 = stark[0].size[0] // 2
     s2 = 290
     font = ImageFont.truetype("./Miku/resources/SuisseIntl-Regular.ttf", 20)
@@ -41,7 +35,18 @@ async def _left_mem(client,message):
         draw = ImageDraw.Draw(stark[i])
         s4 = (s1 - len(text[i // s3]) * 5, s2)
         draw.text(s4, text[i // s3], font=font, anchor=None)
-    x =  f"welcome{uuid.uuid4()}.gif"
+
+        
+        if i == len(stark) // 2:
+            await asyncio.sleep(2)
+            
+            text = [f"Goodbye {first_name}!"]
+            for j in range(len(stark)):
+                draw = ImageDraw.Draw(stark[j])
+                s4 = (s1 - len(text[j // s3]) * 5, s2)
+                draw.text(s4, text[j // s3], font=font, anchor=None)
+
+    x =  f"goodbye_{uuid.uuid4()}.gif"
     stark[0].save(
         x,
         save_all=True,
@@ -50,5 +55,7 @@ async def _left_mem(client,message):
         duration=150,
         loop=0,
     )
-    await client.send_animation(message.chat.id,x,caption=f"**GoodBye {message.left_chat_member.mention}.Never come back again!**")
+
+    
+    await client.send_animation(message.chat.id, x, caption=f"**Goodbye {message.left_chat_member.mention}. Never come back again!**")
     os.remove(x)
