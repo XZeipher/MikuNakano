ff1aaf74d631ec5116e4b03cc0c56b32da60ff8f
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-28 22:23:01 +0530

    Add files via upload

--- Miku/modules/mongo/nsfw_db.py
+++ Miku/modules/mongo/nsfw_db.py
@@ -1,9 +1,25 @@
+# one like for me guys for this hard work uff
+
+from typing import Dict, List, Union
 from Miku import db
 
 nsfwdb = db.nsfw
 
-async def add_nsfw(chat_id : int):
-    return await nsfwdb.insert_one({"chat_id":chat_id})
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
 
-async def rm_nsfw(chat_id : int):
-    return await nsfwdb.delete_one({"chat_id":chat_id})
+async def nsfw_on(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return nsfwdb.insert_one({"chat_id": chat_id})
