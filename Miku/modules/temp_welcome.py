597fc24a13dd18a1c6e3ab083ac2f6680cedcc93
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-30 08:33:56 +0530

    Add files via upload

--- Miku/modules/temp_welcome.py
+++ Miku/modules/temp_welcome.py
@@ -1,16 +1,31 @@
 from pyrogram import *
+import os
 from Miku import app
-from PIL import Image , ImageDraw , ImageFont
+from config import DEV_USERS
+from PIL import Image,ImageOps,ImageDraw,ImageChops, ImageFont 
 
-async def template(name , username , ids):
-    temp = Image.open("./Miku/resources/IMG_20230723_121050_314.png")
-    name_font = ImageFont.truetype("./Miku/resources/FrescitoRegularPersonalUseRegular-lgmj5.otf",53)
-    username_font = ImageFont.truetype("./Miku/resources/FrescitoRegularPersonalUseRegular-lgmj5.otf",49)
-    id_font = ImageFont.truetype("./Miku/resources/monumentextended-regular.otf",40)
+
+async def circle(pfp, size=(215, 215)):
+    pfp = pfp.resize(size, Image.Resampling.LANCZOS).convert("RGBA")
+    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(pfp.size, Image.Resampling.LANCZOS)
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
+    return pfp
+
+async def template(pic , name , ids , stats):
+    temp = Image.open("./Miku/resources/20230730_081017_0000.png")
+    fonts = ImageFont.truetype("./Miku/resources/NotoSans-ExtraBold.ttf",30)
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(pfp,(139,139))
+    temp.paste(pfp, (592, 117), pfp)
     draw = ImageDraw.Draw(temp)
-    draw.text((318 , 407),name,"white",font=name_font)    
-    draw.text((448 , 517),username,"white",font=username_font)
-    draw.text((218 , 620),str(ids),"white",font=id_font)
+    draw.text((186 , 337),name,"white",font=NAME)   
+    draw.text((127 , 394),str(ids),"white",font=NAME)
+    draw.text((208 , 448),stats,"white",font=NAME)   
     temp.save(f"./Miku/resources/temp{ids}.png")
     return f"./Miku/resources/temp{ids}.png"
     
@@ -21,10 +36,17 @@ async def _weem(_, message):
     count = await _.get_chat_members_count(chat_id) 
     for m in message.new_chat_members:
         name = m.first_name 
-        username = m.username
         mention = m.mention 
-        user_id = m.id
-        welpic = await template(name , username , user_id)
+        user_id = m.id       
+        try:
+            user_photo = m.photo.big_file_id
+            pic = await _.download_media(m.photo.big_file_id,file_name =f"pp{user_id}.png")
+        except AttributeError:
+            pic = "./Miku/resources/profilepic.png"
+        if user_id in DEV_USERS:
+        	stats = "Developer"
+        else:
+        	stats = "User"
+        welpic = await template(pic , name , user_id , stats)
         await _.send_photo(chat_id,welpic,caption=f"**{mention} Welcome Kid!**")
         os.remove(welpic)
-        
