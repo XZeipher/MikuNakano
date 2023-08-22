dc066b6d18876281d6799b0c28ab8dfa5ac071ce
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-22 15:02:18 +0530

    Update draw.py

--- Miku/modules/draw.py
+++ Miku/modules/draw.py
@@ -1,24 +1,23 @@
-from pyrogram import Client,filters
+from pyrogram import Client, filters
 from Miku import ubot
 import httpx
+import asyncio
 
-ub_yext ="""
+ub_text = """
 /eval 
 import requests
-b = requests.post("http://api.qewertyy.me/models/inference/task",data={}).json()
+b = requests.post("http://api.qewertyy.me/models/inference/task", data={}).json()
 await message.reply_photo(b['img_urls'][0])
 """
 
 async def send_task(text):
     async with httpx.AsyncClient() as cli:
-    	ids = cli.post("http://api.qewertyy.me/models/inference",data={"model_id":2,"prompt":text})
+        ids = cli.post("http://api.qewertyy.me/models/inference", data={"model_id": 2, "prompt": text})
         await asyncio.sleep(20)
-        data = {"task_id":a['task_id'],"request_id":a['request_id']}
-        await ubot.send_message("lslkzmdncbcb" , ub_yext.format(data))
-        
+        data = {"task_id": a['task_id'], "request_id": a['request_id']}
+        await ubot.send_message("lslkzmdncbcb", ub_text.format(data))
+
 @Client.on_message(filters.command("draw"))
-async def draw(_,message):
-	text = message.text.split(maxsplit=1)[1]
-	await send_task(text)
-	
-	
+async def draw(_, message):
+    text = message.text.split(maxsplit=1)[1]
+    await send_task(text)
